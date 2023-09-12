8f993a1514a38b13cf3ecab54060753541c7018d
dianabee <dianabee16@gmail.com>
2023-09-12 11:52:11 -0700

    worked on api routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,16 @@
+router = require("express").Router()
+const {getThoughts, 
+getOneThought,
+createThought,
+updateThought,
+deleteThought,
+addReaction,
+deleteReaction,
+} = require("../../controllers/userController")
+
+router.route("/").get(getThoughts).post(createThought)
+router.route("/:thoughtId").get(getOneThought).put(updateThought).delete(deleteThought)
+router.route("/:thoughtId/reactions").post(addReaction)
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction)
+
+module.exports = router
